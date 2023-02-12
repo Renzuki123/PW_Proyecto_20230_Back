e2cb776aabbb13b641ab38084ed63546031eb68b
Renzuki123 <renzosaucedo@hotmail.com>
2023-02-12 13:07:49 -0500

    prueba

---
+++ endpoints/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+import json
+
+# Create your views here.
