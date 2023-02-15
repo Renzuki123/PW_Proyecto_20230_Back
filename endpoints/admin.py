d4f0627531644160c05f430403926b6af3b797cd
Renzo Sebastian Cavero Canchan <renzo.sebastian.cavero.canchan@gmail.com>
2023-02-15 01:54:20 -0500

    Cambios Agregado POr RenzoC

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Categoria)
+admin.site.register(models.Plato)
+admin.site.register(models.Restaurante)
--- endpoints/models.py
+++ endpoints/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+# Modelos creados por Renzo Cavero:
+class User(models.Model):
+    usuario = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    def __str__(self):
+        return self.usuario + " " + self.password
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='platos/')
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.nombre
+
+class Restaurante(models.Model):
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='platos/', default="")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.nombre
+
+
