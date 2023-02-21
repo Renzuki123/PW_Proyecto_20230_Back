160e91e598af45ce3ac8b1e13cb86d824b93cef3
Renzuki123 <renzosaucedo@hotmail.com>
2023-02-21 17:14:24 -0500

    setteando postgresqV1

--- endpoints/models.py
+++ endpoints/models.py
@@ -35,9 +35,19 @@ class Restaurantes(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
     img = models.ImageField(upload_to='platos/')
-    precio = models.DecimalField(max_digits=6, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.nombre
 
+class Pedido(models.Model):
+    PEDIDO_ESTADOS = (
+        ("S", "SOLICITADO"),
+        ("EP", "EN PREPARACION"),
+        ("EC", "EN CAMINO"),
+        ("E", "ENTREGADO")
+    )
 
+class PedidoXPlato(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    plato = models.ForeignKey(Platos, on_delete=models.CASCADE)
+    precio = models.DecimalField(default=0.0, decimal_places=2, max_digits=7)
